d2e31fd934b1bee21ddf9681560c99ff02b88b28
MehKad <mehkadiri@gmail.com>
2023-06-20 14:37:31 +0100

    Add GUI tkinter

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,13 +1,11 @@
 import random
+import tkinter as tk
+from tkinter import messagebox
 
 
 def print_board(board):
-    print("---------")
     for row in board:
-        print("|", end="")
-        for cell in row:
-            print(cell, end="|")
-        print("\n---------")
+        print(row)
 
 
 def check_winner(board, player):
@@ -30,40 +28,58 @@ def check_winner(board, player):
     return False
 
 
-def play_game():
-    board = [[" " for _ in range(3)] for _ in range(3)]
-    players = ["X", "O"]
-    current_player = random.choice(players)
+def make_move(row, col):
+    global board, current_player
 
-    while True:
+    if board[row][col] != " ":
+        messagebox.showerror("Invalid Move", "Invalid move! Try again.")
+        return
+
+    board[row][col] = current_player
+    button = buttons[row][col]
+    button.config(text=current_player, state=tk.DISABLED)
+
+    if check_winner(board, current_player):
         print_board(board)
-
-        print(f"Current player: {current_player}")
-
-        if current_player == "X":
-            row = int(input("Enter row number (0-2): "))
-            col = int(input("Enter column number (0-2): "))
-        else:
-            row = random.randint(0, 2)
-            col = random.randint(0, 2)
-
-        if board[row][col] != " ":
-            print("Invalid move! Try again.")
-            continue
-
-        board[row][col] = current_player
-
-        if check_winner(board, current_player):
-            print_board(board)
-            print(f"Player {current_player} wins!")
-            break
-
-        if all(board[row][col] != " " for row in range(3) for col in range(3)):
-            print_board(board)
-            print("It's a tie!")
-            break
-
+        messagebox.showinfo("Game Over", f"Player {current_player} wins!")
+        reset_game()
+    elif all(board[row][col] != " " for row in range(3) for col in range(3)):
+        print_board(board)
+        messagebox.showinfo("Game Over", "It's a tie!")
+        reset_game()
+    else:
         current_player = "O" if current_player == "X" else "X"
 
 
-play_game()
+def reset_game():
+    global board, current_player
+    board = [[" " for _ in range(3)] for _ in range(3)]
+    current_player = random.choice(["X", "O"])
+
+    for row in range(3):
+        for col in range(3):
+            button = buttons[row][col]
+            button.config(text=" ", state=tk.NORMAL)
+
+
+# Create the main window
+window = tk.Tk()
+window.title("Tic Tac Toe")
+
+# Create the game board buttons
+buttons = []
+for row in range(3):
+    button_row = []
+    for col in range(3):
+        button = tk.Button(window, text=" ", font=("Arial", 20), width=6, height=3,
+                           command=lambda r=row, c=col: make_move(r, c))
+        button.grid(row=row, column=col, padx=5, pady=5)
+        button_row.append(button)
+    buttons.append(button_row)
+
+# Initialize the game
+board = [[" " for _ in range(3)] for _ in range(3)]
+current_player = random.choice(["X", "O"])
+
+# Start the main event loop
+window.mainloop()
